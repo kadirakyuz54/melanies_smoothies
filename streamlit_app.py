00180270fdbd8eb73cd18f0869e8efc6def2c975
Kadir Akyuz <71395584+kadirakyuz54@users.noreply.github.com>
2026-06-18 10:18:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,9 @@ if ingredients_list:
         # EXTRA CREDIT: Appending the customer's name dynamically to the success banner!
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response)
+# Convert the JSON payload into a clean, wide dataframe layout
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
